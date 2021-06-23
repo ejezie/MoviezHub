5ee93296c9c36150b50803572ab4fc6cd7e94608
EJEZIE CHINEDU EMMANUEL <54438361+ejezie@users.noreply.github.com>
2021-06-23 17:50:47 +0100

    banner

--- movies-hub/src/Banner.js
+++ movies-hub/src/Banner.js
@@ -25,7 +25,7 @@ function Banner() {
     return str?.lenght > n ? str.substr(0, n-1) + "..." : str;
   }
 
-  overviewBanner = movie?.overview;
+  const overviewBanner = movie?.overview;
 
   return (
     <header
